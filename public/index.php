99702c5e1c8f6034d8db2fd6109be86bd0f224d5
Kabasele <erickabasele03@gmail.com>
2025-10-04 03:33:25 +0200

    toujours mise à jour des liens style css

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@
 <html lang="fr">
     <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="/appsem/public/style.css">
+    <link rel="stylesheet" href="/public/style.css">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome -->
